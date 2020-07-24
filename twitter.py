9e5cec98d80d575b6fd0f91a1ceaf8f0cf229f5c
Mann Patel <46739555+manncodes@users.noreply.github.com>
2020-07-24 12:48:24 +0530

    +secured

--- twitter.py
+++ twitter.py
@@ -3,10 +3,10 @@ import tweepy
 
 def Tweet(inpStr):
     # personal details 
-    consumer_key ="d2xgptvJEsOTojta8Lz8qeVgx"
-    consumer_secret ="OIsph4tEEBNkYNUWRpqm8WnaBbD4KaCzbmMldKbPB6mPAfy8RD"
-    access_token ="1177841903573692417-qwKLV8HMz2c2Iz34J4NIEib7rPJfgb"
-    access_token_secret ="gGC0ePQyIXj3fUJY7g8kJovVfkkisd0WI8eTUaXg5odIh"
+    consumer_key ="<insert key here>"
+    consumer_secret ="<insert secret here>"
+    access_token ="<insert token here>"
+    access_token_secret ="<insert secret token here>"
   
     # authentication of consumer key and secret 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
