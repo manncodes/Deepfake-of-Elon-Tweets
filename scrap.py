41ed17cf6981b42e99dc07f485dcbad1d755f767
Mann Patel <46739555+manncodes@users.noreply.github.com>
2020-08-14 13:08:28 +0530

    selenium scroller added

--- scrap.py
+++ scrap.py
@@ -1,13 +1,50 @@
 import requests
+from bs4 import BeautifulSoupfrom selenium import webdriver
 from bs4 import BeautifulSoup
+import re
+import time
+import csv
+import io
+import pprint as pp
+
+
+path_to_chromedriver ="C:\\Users\\kishi\\Downloads\\chromedriver_win32\\chromedriver"            #enter path of chromedriver
+browser = webdriver.Chrome(executable_path = path_to_chromedriver)
+
+
+url = input("Enter the url: ")  #eg: https://www.twitter.com/xyz/
+
+#this function is to handle dynamic page content loading - using Selenium
+def tweet_scroller(url):
+
+    browser.get(url)
+    
+    #define initial page height for 'while' loop
+    lastHeight = browser.execute_script("return document.body.scrollHeight")
+    
+    while True:
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        #define how many seconds to wait while dynamic page content loads
+        time.sleep(3)
+        newHeight = browser.execute_script("return document.body.scrollHeight")
+        
+        if newHeight == lastHeight:
+            break
+        else:
+            lastHeight = newHeight
+            
+    html = browser.page_source
+
+    return html
 
 if __name__ == '__main__':
-
+        
         all_tweets = []
 
         url = 'https://twitter.com/elonmusk'
-
-        data = requests.get(url)
+        
+        data  = tweet_scroller(url)
 
         html = BeautifulSoup(data.text, 'html.parser')
 
